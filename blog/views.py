c7279791b48aa9a6ac5e96be8c9f596ec2bb7109
Mateus Castro <mateus.castro.domingos@gmail.com>
2020-03-11 09:36:19 -0300

    API implementada

--- blog/views.py
+++ blog/views.py
@@ -2,15 +2,14 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
+from rest_framework import generics
 from .models import Post
 from .forms import PostForm
+from .serializers import PostSerializer
 
 def post_list(request): 
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
     return render(request, 'blog/post_list.html', {'posts': posts})
-
-def post_detail (request, pk):
-    Post.objects.get(pk=pk)
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -42,3 +41,7 @@ def post_edit(request, pk):
      else:
          form = PostForm(instance=post)
      return render(request, 'blog/post_edit.html', {'form': form})
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
